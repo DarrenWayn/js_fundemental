f8827048fba483913934beaed2ccf0c011269494
Darren Wayn <darrenwayn98@gmail.com>
2022-07-16 15:30:18 +0700

    Challenge Completed

--- script.js
+++ script.js
@@ -894,12 +894,16 @@ const rest2 = {
 // rest2.numGuests = rest2.numGuests || 10;
 
 // assign a varaibles if the current one is currently falsy
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
 
+// AND assignment operator
 // rest2.owner is truthy value so <ANONYMOUS> get executed with AND OPERATOR
-rest1.owner = rest1.owner && '<ANONYMOUS>';
-rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
 
 // nullish assignment operator (null, undefined)
 rest1.numGuests ??= 10;
