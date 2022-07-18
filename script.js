bb2bdb66b821bbec8573d6362789732a7f10d671
Darren Wayn <darrenwayn98@gmail.com>
2022-07-18 14:01:21 +0700

    Coding Challenge 2 completed

--- script.js
+++ script.js
@@ -878,28 +878,28 @@ const restaurants = {
 };
 
 // property name
-const properties = Object.keys(openingHours);
-console.log(properties);
+// const properties = Object.keys(openingHours);
+// console.log(properties);
 
-let openStr = `We are open on ${properties.length} days: `;
+// let openStr = `We are open on ${properties.length} days: `;
 
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-console.log(openStr);
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
 
-// property values
-const values = Object.values(openingHours);
-console.log(values);
+// // property values
+// const values = Object.values(openingHours);
+// console.log(values);
 
-// Entries that turn object into arrays
-const entries = Object.entries(openingHours);
-// console.log(entries);
+// // Entries that turn object into arrays
+// const entries = Object.entries(openingHours);
+// // console.log(entries);
 
-// [key, value]
-for (const [day, { open, close }] of entries) {
-  console.log(`On ${day} we open at ${open} and close at ${close}`);
-}
+// // [key, value]
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
+// }
 
 // if (restaurants.openingHours && restaurants.openingHours.mon)
 //   console.log(restaurants.openingHours.mon.open);
