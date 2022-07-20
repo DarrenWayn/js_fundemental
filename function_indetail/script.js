6b507449ffaef0d569ab870809db92572a82cb47
Darren Wayn <darrenwayn98@gmail.com>
2022-07-20 23:26:32 +0700

    callback function done

---
+++ function_indetail/script.js
@@ -0,0 +1,89 @@
+'use strict';
+
+// const bookingArray = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassanger = 1,
+//   price = 199 * numPassanger
+// ) {
+//   // ES5
+//   //   numPassanger = numPassanger || 1;
+//   //   price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassanger,
+//     price,
+//   };
+
+//   console.log(booking);
+//   bookingArray.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+
+// createBooking('LH123', undefined,  1000);
+
+// const flight = 'LH234';
+// const darren = {
+//   name: 'Darren Wayn',
+//   passport: 2372349598,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.name === 2372349598) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong Passport!');
+//   }
+// };
+
+// // checkIn(flight, darren);
+// // console.log(flight);
+// // console.log(darren);
+
+// // // Is the same as doing ...
+// // const flightNum = flight;
+// // const passenger = darren;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(darren);
+// console.log(flight, darren);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, ''.toLowerCase());
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transform = function (str, fn) {
+  console.log(`Orginal string: ${str}`);
+  console.log(`Transform string: ${fn(str)}`);
+
+  console.log(`Transform by ${fn.name}`);
+};
+
+transform('Javascript is the best!', upperFirstWord);
+transform('Javascript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('Hi');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
