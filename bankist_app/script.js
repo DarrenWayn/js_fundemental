dacf89eaf752a512e7dd941157cdecc9b014808c
Darren Wayn <darrenwayn98@gmail.com>
2022-08-15 23:15:37 +0700

    added bankist dom

--- bankist_app/script.js
+++ bankist_app/script.js
@@ -257,32 +257,52 @@ btnSort.addEventListener('click', function (e) {
 });
 
 // Conversion
-console.log(Number('23'));
-console.log(+'23');
+// console.log(Number('23'));
+// console.log(+'23');
 
 // Parsing
-console.log(Number.parseInt('30px', 10));
-console.log(Number.parseInt('e23', 10));
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e23', 10));
 
-console.log(Number.parseInt('  2.5rem  '));
-console.log(Number.parseFloat('  2.5rem  '));
+// console.log(Number.parseInt('  2.5rem  '));
+// console.log(Number.parseFloat('  2.5rem  '));
 
 // Checking the values is not a number NaN
-console.log(Number.isNaN(20));
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(+'20'));
-console.log(Number.isNaN(23 / 0));
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20'));
+// console.log(Number.isNaN(23 / 0));
 
 // Checking the values is number is the best using isFinite
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20X'));
-console.log(Number.isFinite(23 / 0));
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
 
-console.log(Number.isInteger(20));
-console.log(Number.isInteger('20'));
-console.log(Number.isInteger(+'20X'));
-console.log(Number.isInteger(23 / 0));
+// console.log(Number.isInteger(20));
+// console.log(Number.isInteger('20'));
+// console.log(Number.isInteger(+'20X'));
+// console.log(Number.isInteger(23 / 0));
+
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// console.log(Math.max(8, 18, 23, 11, 2));
+// console.log(Math.max(8, 18, '23', 11, 2));
+// console.log(Math.max(8, 18, '23px', 11, 2));
+
+// console.log(Math.min(8, 18, 23, 11, 2));
+// console.log(Math.min(8, 18, '23', 11, 2));
+// console.log(Math.min(8, 18, '23px', 11, 2));
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.Trunc(Math.random() * 6) + 1);
+
+// const randomInt = (min, max) => Math.trunc(Math.random() + (max - min) + 1);
+// 0...1 => 0...(max-min) -> min...max
+// console.log(randomInt(10, 20));
 
 // const arr = [1, 2, 3, 4, 5, 6, 7];
 // console.log(new Array(1, 2, 3, 4, 5, 6, 7));
