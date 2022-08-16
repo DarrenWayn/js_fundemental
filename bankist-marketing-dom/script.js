9b8d3b2ca58a65013ab22a0ed0a26112b9e66b49
Darren Wayn <darrenwayn98@gmail.com>
2022-08-16 16:05:41 +0700

    learning attribute in manipulating DOM

--- bankist-marketing-dom/script.js
+++ bankist-marketing-dom/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,34 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// // Attribute
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo';
+
+// // Non-Standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'Bankist');
+
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// // Data Attributes
+// console.log(logo.dataset.versionNumber);
+
+// // Classes
+// logo.classList.add('c', 'j')
+// logo.classList.remove('c', 'j')
+// logo.classList.toggle('c');
+// logo.classList.contain('c'); // not includes like in the array
+
+// // Don't Use
+// logo.className = 'Jonas'
